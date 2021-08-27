1efec45d648583fceee3f4d3790b732b606c0090
Dinesh Nagarajan <dineshnagarajan19@gmail.com>
2021-08-28 00:02:42 +0530

    spelling update for columns

--- initial_103.py
+++ initial_103.py
@@ -5,15 +5,19 @@ import pandas as pd
 titanic = pd.read_csv("data/titanic.csv")
 
 print(titanic.head())
-# Get single coloums in the DF
+# Get single columns in the DF
 ages = titanic["Age"]
-# Get multiple coloums in the DF
+# Get multiple columns in the DF
 ages_sex = titanic[["Age", "Sex"]]
 
-print("****** single coloums in the DF ******")
+# Single columns selection in DF
+print("****** single columns in the DF ******")
 print(ages.head())
-print("****** multiple coloums in the DF ******")
+
+# Multiple columns
+print("****** multiple columns in the DF ******")
 print(ages_sex.head())
-# Give number of rows and coloumns
-print("****** number of rows and coloumns ******")
+
+# Give number of rows and columns
+print("****** number of rows and columns ******")
 print(ages_sex.shape)
